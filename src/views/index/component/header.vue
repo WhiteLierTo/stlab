ae125e306231387ff268afeb80eadd2c48cde58e
lichao <1932018667@qq.com>
2020-10-21 17:23:48 +0800

    头部

--- src/views/index/component/header.vue
+++ src/views/index/component/header.vue
@@ -13,7 +13,18 @@
         text-color="#fff"
         active-text-color="#ffd04b"
       >
-        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="1">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+          <el-menu-item index="1-3">选项3</el-menu-item>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+            <el-menu-item index="1-4-2">选项2</el-menu-item>
+            <el-menu-item index="1-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
         <el-submenu index="2">
           <template slot="title">我的工作台</template>
           <el-menu-item index="2-1">选项1</el-menu-item>
@@ -26,12 +37,30 @@
             <el-menu-item index="2-4-3">选项3</el-menu-item>
           </el-submenu>
         </el-submenu>
-        <el-menu-item index="3" disabled>消息中心</el-menu-item>
-        <el-menu-item index="4"
-          ><a href="https://www.ele.me" target="_blank"
-            >订单管理</a
-          ></el-menu-item
-        >
+         <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
       </el-menu>
     </div>
     <div class="header-right"></div>
@@ -80,10 +109,13 @@ export default {
 </style>
 
 <style lang="less">
-.saddaadsads{
-  .el-submenu__title{
+.saddaadsads {
+  .el-submenu .el-submenu__title {
     height: 75px;
     line-height: 75px;
+  }
+  .el-menu.el-menu--horizontal {
+    border-bottom: none;
   }
 }
 </style>
